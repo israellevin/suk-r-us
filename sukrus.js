8770ff41bd4f91bc4e394475d5fb67fa52352375
Israel <root@boltage>
2020-07-11 19:36:29 +0300

    Add custom marker

--- sukrus.js
+++ sukrus.js
@@ -1,14 +1,17 @@
-// jshint esversion: 8
+/* jshint esversion: 8 */
+/* globals google */
 (function(exposed){'use strict';
 
 // Initialization callback called from google once the API is done loading, so it has to be exposed.
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {zoom:15});
     navigator.geolocation.getCurrentPosition(function(position){
-        map.setCenter({
+        let current = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
-        });
+        };
+        map.setCenter(current);
+        new google.maps.Marker({position: current, icon: 'sup.png', map: map});
     });
 }
 
